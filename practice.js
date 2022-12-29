f9eba519614ca3944aa7203f0f783e18bb8d854f
Lynn Ventura <m_lynnventura@yahoo.com>
2022-12-29 12:16:27 -0800

    added for loop and conditionals to complete algo practice

--- practice.js
+++ practice.js
@@ -1,6 +1,11 @@
-var countPositives = 0;
+var countPositives = 0; // provides the sum of positive # in the array
 var arr = [3, 4, -2, 7, 16, -8, 0];
     
-//your code here
+for (var i=0; i<arr.length; i++) {
+    if (arr[i] >= 0) {
+        countPositives += 1;
+        console.log(arr[i]);
+    }
+}
     
 console.log("there are " + countPositives + " positive values");
